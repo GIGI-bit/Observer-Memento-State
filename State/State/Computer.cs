0a84fbdd978df1ecd483178c943b5972eec9919e
Sevgi <64860005+GIGI-bit@users.noreply.github.com>
2023-12-13 23:04:15 +0400

    Add files via upload

---
+++ State/State/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Computer
+    {
+        private State state;
+        public int Volume;
+        public Computer()
+        {
+            
+        }
+
+        public void turnOff() { }
+
+        public void turnOn() { }
+        public void volumeUp(int volume) { Volume += volume; }
+        public void volumeDown(int volume) { Volume -= volume; }
+    }
+}
---
+++ State/State/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public abstract class State
+    {
+        protected Computer comp;
+        public State()
+        {
+            
+        }
+        protected abstract void click();
+        protected abstract void clickToOpen();
+        protected abstract void clickToClose();
+    }
+}
